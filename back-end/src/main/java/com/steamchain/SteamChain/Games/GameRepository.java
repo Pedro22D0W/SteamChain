be89a7f85dbae94b756636cd91e7c055965ea842
Pedro22D0W <pedro.rodolfo.galvao22@hotmail.com>
2023-12-08 14:21:28 -0300

    userGames e verify

--- back-end/src/main/java/com/steamchain/SteamChain/Games/GameRepository.java
+++ back-end/src/main/java/com/steamchain/SteamChain/Games/GameRepository.java
@@ -1,9 +1,14 @@
 package com.steamchain.SteamChain.Games;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.steamchain.SteamChain.User.User;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
    Game findByid(long id);
+   List<Game> findByUsers(User user);
 }
 
--- back-end/src/main/java/com/steamchain/SteamChain/controller/UserController.java
+++ back-end/src/main/java/com/steamchain/SteamChain/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.steamchain.SteamChain.controller;
+import com.steamchain.SteamChain.Games.Game;
+import com.steamchain.SteamChain.Games.GameRepository;
+import com.steamchain.SteamChain.Games.GameResponseDTO;
 import com.steamchain.SteamChain.User.*;
 import com.steamchain.SteamChain.servicies.UserService;
 import jakarta.annotation.security.PermitAll;
 import jakarta.transaction.Transactional;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -17,6 +23,8 @@ public class UserController {
     private UserRepository repository;
     @Autowired
     private UserService userService;
+    @Autowired
+    private GameRepository GameRepository;
 
     @GetMapping("/developer")
     public ResponseEntity<UserResponseDTO> obterDadosDoBanco(Authentication authentication) {
@@ -35,6 +43,27 @@ public class UserController {
         return "Compra realizada com sucesso!";
     }
 
+    @GetMapping("/usergames/{user_id}")
+    @Transactional
+        public List<GameResponseDTO> userGames(@PathVariable Long user_id){
+            User user = repository.findByid(user_id);
+            List<GameResponseDTO> Games_List = GameRepository.findByUsers(user).stream().map(GameResponseDTO::new).toList();
+            return Games_List;
+        }
+        @GetMapping("/verifygame/{user_id}/{game_id}")
+    @Transactional
+        public Boolean userVerifyGame(@PathVariable Long user_id,@PathVariable Long game_id){
+            User user = repository.findByid(user_id);
+            Game game = GameRepository.findByid(game_id);
+           
+            if(GameRepository.findByUsers(user).contains(game)){
+                return true;
+            }
+            else{
+                return false;
+            }    
+        }
+    
     @PutMapping
     @Transactional
     public void update(@RequestBody UserUpdateDTO user_update) {
